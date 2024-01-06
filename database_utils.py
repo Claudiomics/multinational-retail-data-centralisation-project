12ea0200d3de6c2dc9266ab09818addd7f4c5019
Claudiomics <c.carnell11@hotmail.com>
2024-01-06 23:45:29 +0000

    Adds read_db_creds, init_db_engine and list_db_engine methods to the DatabaseConnector class. Main.py uses them to connect to the database. Adds db_creds.yaml file.

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,7 @@
-
+import yaml
+from sqlalchemy import create_engine
+from sqlalchemy import text
+from sqlalchemy import inspect
 
 class DatabaseConnector:
     '''
@@ -7,3 +10,48 @@ class DatabaseConnector:
     Methods:
     --------
     ''' 
+
+    # This method returns a yaml file as a dictionary
+    def read_db_creds(self, file):
+        
+        # Open YAML file in read mode
+        with open(file, 'r') as stream: 
+            # Use PyYAML's safe_load to parse the YAML content into a Python dictionary
+            data_loaded = yaml.safe_load(stream)
+    
+        return data_loaded
+    
+    # This method uses the yaml dictionary to create a sqlachemy database engine
+    def init_db_engine(self, file):
+        
+        # Read database credentials from the specified YAML file
+        dict_yaml_func = self.read_db_creds(file)
+
+        # Create a SQLAlchemy engine using the database credentials
+        engine = create_engine(f"postgresql+psycopg2://{dict_yaml_func['USER']}:{dict_yaml_func['PASSWORD']}@{dict_yaml_func['HOST']}:{dict_yaml_func['PORT']}/{dict_yaml_func['DATABASE']}")
+        # Connect to the database using the engine
+        engine.connect()
+        
+        return engine
+    
+    # This method lists all the tables in the database to identify tables for data extraction
+    def list_db_tables(self, file):
+
+        # Create the database engine
+        engine = self.init_db_engine(file) 
+        # Inspect the structure of the database
+        inspector = inspect(engine)
+        # Get a list of table names in the 'public' schema
+        table_names = inspector.get_table_names() 
+
+        # Connect to the database and use an SQL query to retrieve table names
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"))
+            list_of_tables = []
+            for table in result:
+                list_of_tables.append(table)
+        unpacked_tuples_list = [table[0] for table in list_of_tables]
+        
+        return unpacked_tuples_list
+    
+    
--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+#from data_cleaning import DatabaseCleaning
+#from data_extraction import DataExtractor
+from database_utils import DatabaseConnector
+import yaml
+
+
 ''' This is the script where I will use the three different classes (DatabaseConnector, 
 DataExtractor and DatabaseCleaning) to retrive data from a variety of sources, clean 
 it and upload a dataframe to the sales_data database in the relational database management 
 system PostgreSQL.'''
 
+### Creating a connection to the AWS database
+
+## Create an instance of DatabaseConnector class
+database_connector = DatabaseConnector()
+
+## Use read_db_creds mehtod to return yaml credentials in useable format
+yaml_creds = database_connector.read_db_creds('db_creds.yaml')
+#print(yaml_creds) 
+
+## Use these credentials to create an SQLAlchemy database engire to connect to the RDS
+engine = database_connector.init_db_engine('db_creds.yaml')
+#print(engine)
+
+## List the table names that are in the AWS RDS database
+get_table_names = database_connector.list_db_tables('db_creds.yaml')
+print(get_table_names) # ['legacy_store_details', 'legacy_users', 'orders_table']
 
 
 
