1dfa8f69f8209359a3650e3cee6a0b61b025efcf
Claudiomics <c.carnell11@hotmail.com>
2024-01-10 11:55:08 +0000

    Re-structures main.py to increase readability. Puts sql files into seperate folder.

--- main.py
+++ main.py
@@ -13,21 +13,7 @@ from data_extraction import DataExtractor
 DataExtractor and DatabaseCleaning) to retrive data from a variety of sources, clean 
 the data and upload to the sales_data database in the relational database management 
 system PostgreSQL. Finally, it will run two sql scripts which will create the database schema 
-and run queries on it.'''
-
-### 1. Creating a connection to the AWS database
-
-# Create an instance of DatabaseConnector class
-database_connector = DatabaseConnector()
-# Use read_db_creds mehtod to return yaml credentials in useable format
-yaml_creds = database_connector.read_db_creds('db_creds.yaml')
-# Use these credentials to create an SQLAlchemy database engire to connect to the RDS
-engine = database_connector.init_db_engine('db_creds.yaml')
-# List the table names that are in the AWS RDS database
-get_table_names = database_connector.list_db_tables('db_creds.yaml')
-#print(get_table_names) # ['legacy_store_details', 'legacy_users', 'orders_table']
-
-### 2. Retrive, clean and upload the user data from an AWS database in the cloud.
+and run business queries on it, visualising one query with a piechart.'''
 
 def user_data():
     '''
@@ -52,9 +38,6 @@ def user_data():
     database_connector.upload_to_db(clean_user_df, "dim_users", 'my_creds.yaml')
     return clean_user_df
 
-
-### 3. Retrive, clean and upload the card data from a PDF document in an AWS S3 bucket
-
 def card_data():
     '''
     This function retrieves, cleans, and uploads card data from a PDF document in an AWS S3 bucket into a new table in the sales_data database.
@@ -77,24 +60,6 @@ def card_data():
     
     return clean_card_df
 
-
-### 4. Extract, clean and upload store details from using an API
-
-'''The store data can be retrieved through the use of an API.
-The API has two GET methods. One will return the number of stores in the business and the other to retrieve a store given a store number.
-
-The two endpoints for the API are as follows:
-Retrieve a store: https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}
-Return the number of stores: https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'''
-
-# Use read_db_creds method to read yaml file with api key
-api_header_details = database_connector.read_db_creds('api_key.yaml')
-num_of_stores_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
-api_retrieval = DataExtractor()
-# Use the list_number_of_stores method to get the number of total stores
-total_stores = api_retrieval.list_number_of_stores(num_of_stores_endpoint, api_header_details)
-#print(total_stores) # 451
-
 def stores_data():
     '''
     This function retrieves, cleans, and uploads stores data using an API into a new table in the sales_data database.
@@ -114,9 +79,6 @@ def stores_data():
     database_connector.upload_to_db(clean_store_df, "dim_store_details", 'my_creds.yaml')
 
     return clean_store_df
-
-
-### 5. Extract, edit, clean and upload data from product details from csv file in s3 bucket
 
 def product_data():
     '''
@@ -144,9 +106,6 @@ def product_data():
 
     return cleaned_product_df
 
-
-### 6. Retrieve orders table from AWS RDS
-
 def orders_data():
     '''
     This function retrieves, cleans, and uploads orders data from an AWS RDS into a new table in the sales_data database.
@@ -169,9 +128,6 @@ def orders_data():
     database_connector.upload_to_db(orders_df, "orders_table", 'my_creds.yaml')
 
     return orders_df
-
-
-### 7. Retrieve, clean and upload date events data from JSON file in s3 bucket.
 
 def date_data():
     '''
@@ -198,17 +154,6 @@ def date_data():
 
     return clean_date_df
 
-date_data()
-orders_data()
-product_data()
-stores_data()
-card_data()
-user_data()
-
-### 8. Create the Database Schema
-# This will be run using the create_schema.sql file and includes casting column datatypes, adding descriptive columns, assigning primary and foreign keys.
-
-# This function runs the input schema sql script
 def execute_schema_sql_file(creds, file_path):
     '''
     This function takes the SQL script from the given file to create the database schema.
@@ -241,19 +186,6 @@ def execute_schema_sql_file(creds, file_path):
         cursor.close()
         conn.close()
 
-# Create connection to postgresql
-postgres_creds = database_connector.read_db_creds('my_creds.yaml')
-connection_str = f"host={postgres_creds['HOST']} dbname={postgres_creds['DATABASE']} user={postgres_creds['USER']} password={postgres_creds['PASSWORD']}"
-
-# Specify SQL file
-schema_sql_file_path = 'create_schema.sql'
-# Run function to create the schema
-execute_schema_sql_file(connection_str, schema_sql_file_path)
-
-### 9. Query the Database  
-# Answer business questions about the sales 
-
-# This function queries the created database and prints the output of sql queries relating to the business
 def execute_query_sql_file(creds, file_path):
     '''
     This function takes the SQL script from the given file and prints the results of the queries.
@@ -313,14 +245,6 @@ def execute_query_sql_file(creds, file_path):
         cursor.close()
         conn.close()
 
-# Specify SQL file
-query_sql_file_path = 'business_queries.sql'
-# Run function to query the database
-execute_query_sql_file(connection_str, query_sql_file_path)
-
-
-# Here's a visual representation of the result of queary 5: What percentage of sales come through each type of store?
-
 def storetype_sales_piechart():
     '''
     This function generates a pie chart using Matplotlib to visualize the percentage of sales that come through each type of store in the database.
@@ -347,6 +271,65 @@ def storetype_sales_piechart():
 
     plt.show()
 
-storetype_sales_piechart()
 
+if __name__ == "__main__":
+    ### 1. Creating a connection to the AWS database
 
+    # Create an instance of DatabaseConnector class
+    database_connector = DatabaseConnector()
+    # Use read_db_creds mehtod to return yaml credentials in useable format
+    yaml_creds = database_connector.read_db_creds('db_creds.yaml')
+    # Use these credentials to create an SQLAlchemy database engire to connect to the RDS
+    engine = database_connector.init_db_engine('db_creds.yaml')
+    # List the table names that are in the AWS RDS database
+    get_table_names = database_connector.list_db_tables('db_creds.yaml')
+    #print(get_table_names) # ['legacy_store_details', 'legacy_users', 'orders_table']
+
+    ### 2. Retrive, clean and upload the user data from an AWS database in the cloud.
+    user_data()
+    ### 3. Retrive, clean and upload the card data from a PDF document in an AWS S3 bucket
+    card_data()
+    ### 4. Extract, clean and upload store details using an API
+    '''The store data can be retrieved through the use of an API.
+    The API has two GET methods. One will return the number of stores in the business and the other to retrieve a store given a store number.
+
+    The two endpoints for the API are as follows:
+    Retrieve a store: https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}
+    Return the number of stores: https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'''
+    # Use read_db_creds method to read yaml file with api key
+    api_header_details = database_connector.read_db_creds('api_key.yaml')
+    num_of_stores_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
+    api_retrieval = DataExtractor()
+    # Use the list_number_of_stores method to get the number of total stores
+    total_stores = api_retrieval.list_number_of_stores(num_of_stores_endpoint, api_header_details)
+    #print(total_stores) # 451
+    stores_data()
+    ### 5. Extract, edit, clean and upload data from product details from csv file in s3 bucket
+    product_data()
+    ### 6. Retrieve orders table from AWS RDS
+    orders_data()
+    ### 7. Retrieve, clean and upload date events data from JSON file in s3 bucket.
+    date_data()   
+
+    ### 8. Create the Database Schema
+    # This will be run using the create_schema.sql file and includes casting column datatypes, adding descriptive columns, assigning primary and foreign keys.
+
+    # Create connection to postgresql
+    postgres_creds = database_connector.read_db_creds('my_creds.yaml')
+    connection_str = f"host={postgres_creds['HOST']} dbname={postgres_creds['DATABASE']} user={postgres_creds['USER']} password={postgres_creds['PASSWORD']}"
+
+    # Specify SQL file
+    schema_sql_file_path = 'sql_files/essential_queries/create_schema.sql'
+    # Run function to create the schema
+    execute_schema_sql_file(connection_str, schema_sql_file_path)
+
+    ### 9. Query the Database  
+    # Answering business questions about sales 
+
+    # Specify SQL file
+    query_sql_file_path = 'sql_files/essential_queries/business_queries_copy.sql'
+    # Run function to query the database
+    execute_query_sql_file(connection_str, query_sql_file_path)
+
+    # Here's a visual representation of the result of queary 5: What percentage of sales come through each type of store?
+    storetype_sales_piechart()
